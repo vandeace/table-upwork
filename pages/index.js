71cb4b5f4f6a777f78d8c0a18b574a67232f97c0
Muhammad Jafar Ash Siddiq <muhammadfajar@stmik-amik-riau.ac.id>
2021-09-17 18:36:30 +0700

    finish component

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,134 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className='flex flex-col min-h-screen bg-black-bg'>
+      <div className='flex flex-row justify-between items-center p-10'>
+        <h5 className='text-white'>LOANS</h5>
+        <h6 className='text-green'>ALL LOANS</h6>
+      </div>
+      <div className='w-full h-3/4 p-10 flex flex-row'>
+        <div className='w-full h-full'>
+          <h2 className='text-white text-center'>Borrowed</h2>
+          <div className='w-full mt-5'>
+            <div className='table-fixed w-full text-left'>
+              <div>
+                <div className='font-thin text-white w-full h-10 flex flex-row my-2'>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    Borrowed
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    Collateral
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    Repaid
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    Annual Rate
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    Duration
+                  </div>
+                  <div className='flex justify-start items-center w-1/6 '>
+                    Payment Progress
+                  </div>
+                </div>
+              </div>
+              <div className='flex flex-col'>
+                <div className='bg-black-item text-white w-full rounded-full border-grey h-12 flex flex-row my-2 border-t-4 '>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    1,000.00 <b className='pl-1'>BUSD</b>
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    2.0 <b className='pl-1'>BUSD</b>
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    200.00<b className='pl-1'>BUSD</b>
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    10.00%
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    5 Days
+                  </div>
+                  <div className='flex justify-between items-center w-1/6 '>
+                    <div className='flex justify-between items-center w-3/6 relative '>
+                      <div className='bg-green h-2 w-20 rounded-full absolute z-20' />
+                      <div className='bg-grey h-2 w-20 rounded-full absolute z-10' />
+                    </div>
+                    <div className='w-1/6'>50%</div>
+                    <div className='flex flex-col w-1/6'>
+                      <div className='h-2 w-2 rounded-full bg-semi mb-1' />
+                      <div className='h-2 w-2 rounded-full bg-semi mb-1' />
+                      <div className='h-2 w-2 rounded-full bg-semi mb-1' />
+                    </div>
+                  </div>
+                </div>
+                <div className='bg-black-item text-white w-full rounded-full border-red h-12 flex flex-row my-2 border-t-4 '>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    1,000.00 <b className='pl-1'>BUSD</b>
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    2.0 <b className='pl-1'>BUSD</b>
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    200.00<b className='pl-1'>BUSD</b>
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    10.00%
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    5 Days
+                  </div>
+                  <div className='flex justify-between items-center w-1/6 '>
+                    <div className='flex justify-between items-center w-3/6 relative '>
+                      <div className='bg-green h-2 w-16 rounded-full absolute z-20' />
+                      <div className='bg-grey h-2 w-20 rounded-full absolute z-10' />
+                    </div>
+                    <div className='w-1/6'>75%</div>
+                    <div className='flex flex-col w-1/6'>
+                      <div className='h-2 w-2 rounded-full bg-semi mb-1' />
+                      <div className='h-2 w-2 rounded-full bg-semi mb-1' />
+                      <div className='h-2 w-2 rounded-full bg-semi mb-1' />
+                    </div>
+                  </div>
+                </div>
+                {/* third row */}
+                <div className='bg-black-item text-white w-full rounded-full border-grey h-12 flex flex-row my-2 border-t-4 '>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    1,000.00 <b className='pl-1'>BUSD</b>
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    2.0 <b className='pl-1'>BUSD</b>
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    200.00<b className='pl-1'>BUSD</b>
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    10.00%
+                  </div>
+                  <div className='flex justify-center items-center w-1/6 '>
+                    5 Days
+                  </div>
+                  <div className='flex justify-between items-center w-1/6 '>
+                    <div className='flex justify-between items-center w-3/6 relative '>
+                      <div className='bg-green h-2 w-5 rounded-full absolute z-20' />
+                      <div className='bg-grey h-2 w-20 rounded-full absolute z-10' />
+                    </div>
+                    <div className='w-1/6'>25%</div>
+                    <div className='flex flex-col w-1/6'>
+                      <div className='h-2 w-2 rounded-full bg-semi mb-1' />
+                      <div className='h-2 w-2 rounded-full bg-semi mb-1' />
+                      <div className='h-2 w-2 rounded-full bg-semi mb-1' />
+                    </div>
+                  </div>
+                </div>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                {/* end row */}
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        {/* <RightItem /> */}
+      </div>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,18 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      'black-bg': '#212124',
+      'black-item': '#313135',
+      green: '#B2E748',
+      red: '#D13F3D',
+      grey: '#3E3E3F',
+      white: '#fff',
+      semi: '#888888',
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
